9fcdd56d46f8062abfb7a11b6f11a2412dffb4dc
TKJ <josephkteresa@gmail.com>
2023-06-01 00:18:48 -0700

    starting add_food and view_nutrition pages + back buttons

--- apps/nutrition/static/js/index.js
+++ apps/nutrition/static/js/index.js
@@ -9,7 +9,10 @@ let init = (app) => {
     // This is the Vue data.
     app.data = {
         plate: [],
-        total: []
+        total: [],
+        main_page_mode: true,
+        add_food_mode: false,
+        view_nutrition_mode: false,
     };    
 
     app.enumerate = (a) => {
@@ -29,20 +32,33 @@ let init = (app) => {
         console.log('in remove');
     }
     
+    app.main_page = function(){
+        app.data.main_page_mode = true;
+        app.data.add_food_mode = false;
+        app.data.view_nutrition_mode = false;
+    }
+    
     app.add_entry = function(){
         //complete
         console.log('in add');
+        app.data.main_page_mode = false;
+        app.data.add_food_mode = true;
+        app.data.view_nutrition_mode = false;
     }
     
     app.view_info = function(){
         //complete
         console.log('in view info');
+        app.data.main_page_mode = false;
+        app.data.add_food_mode = false;
+        app.data.view_nutrition_mode = true;
     }
 
     // This contains all the methods.
     app.methods = {
         edit_entry: app.edit_entry,
         remove_entry: app.remove_entry,
+        main_page: app.main_page,
         add_entry: app.add_entry,
         view_info: app.view_info
     };
